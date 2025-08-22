c9b5f9413eca81a9e775668356720213260257b1
Harold <harold.gallice@gmail.com>
2025-08-22 14:57:35 +0200

    [HG] remove exploring of the gold layer

--- dbt_project/models/gold/gold_paris_events_7_next_days.sql
+++ dbt_project/models/gold/gold_paris_events_7_next_days.sql
@@ -4,6 +4,6 @@
 ) }}
 
 SELECT *
-FROM {{ ref('gold_paris_events_daily') }}
+FROM {{ ref('silver_exploring_view') }}
 WHERE DATE(event_date) BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY)
 AND event_duration < 7
