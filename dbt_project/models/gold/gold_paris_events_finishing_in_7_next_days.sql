b340559aba70f0e18ce7f31184b8827d90732be5
Harold <harold.gallice@gmail.com>
2025-08-25 12:05:48 +0200

    [HG] change the target of the gold next 7 days into events ginishing in the next 7 days

--- dbt_project/models/gold/gold_paris_events_finishing_in_7_next_days.sql
+++ dbt_project/models/gold/gold_paris_events_finishing_in_7_next_days.sql
@@ -6,4 +6,4 @@
 SELECT *
 FROM {{ ref('silver_exploring_view') }}
 WHERE DATE(event_date) BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY)
-AND event_duration < 7
+AND event_date_end <= DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY)
