8cc046c6957fc226ffe95170fd810900eadb5bb6
Harold <harold.gallice@gmail.com>
2025-08-27 12:10:49 +0200

    [HG] add working dlt pipleine to fetch data

---
+++ dlt/rest_api_pipeline.py
@@ -0,0 +1,46 @@
+import dlt
+from dlt.sources.rest_api import rest_api_source
+
+def load_paris_events_data() -> None:
+    pipeline = dlt.pipeline(
+        pipeline_name="rest_api_paris_events_data",
+        destination='bigquery',
+        dataset_name="dlt_export"
+    )
+
+    paris_events_data_source = rest_api_source(
+        {
+            "client": {
+                "base_url": "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/exports/"
+            },
+            "resource_defaults": {
+                "endpoint": {
+                    "params": {
+                        "limit": 1000,
+                    },
+                },
+            },
+            "resources": [
+                {
+                    "name": "json",
+                    "endpoint": {
+                        "path": "json",
+                        "params": {
+                            "limit": 1000,
+                            "select": "id,url,title,lead_text,description,date_start,date_end",
+                            "where": 'updated_at > "2025-01-01T00:00:00+00:00"',
+                        },
+                    },
+                    "write_disposition": "replace",
+                }
+            ],
+        }
+    )
+
+    load_info = pipeline.run(paris_events_data_source)
+    print(load_info)
+
+
+if __name__ == "__main__":
+    load_paris_events_data()
+    pass
